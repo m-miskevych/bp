a798862d8d2b2b56920fcd7021cdd8b6e6d15a55
m-miskevych <miskevychmarianna@gmail.com>
2025-03-01 17:12:18 +0100

    admin can create new clients

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,8 +11,29 @@ class Admin::UsersController < ApplicationController
     @client = @admin.users.find(params[:id])
   end
 
+  def new
+    @client = User.new
+  end
+
+  def create
+    @client = User.new(user_params)
+    @client.role = :user  # set role as client
+    @client.admin = @admin  # assign to current admin
+    @client.password = SecureRandom.hex(10) # set temporery password for user ????
+
+    if @client.save
+      redirect_to admin_users_path, notice: t("notices.user_created")
+    else
+      render "new", status: :unprocessable_entity
+    end
+  end
+
   private
   def set_admin
     @admin = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
