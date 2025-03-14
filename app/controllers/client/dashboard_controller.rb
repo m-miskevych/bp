af49f680f7e916d49d71d1bcb213ca9b7dc764f1
m-miskevych <miskevychmarianna@gmail.com>
2025-03-14 20:01:59 +0100

    feat: client dashboard

--- app/controllers/client/dashboard_controller.rb
+++ app/controllers/client/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Client::DashboardController < ApplicationController
   include Authorization
   before_action :authorize_client
 
-  def index; end
+  def index
+    @user_plans_draft = current_user.user_plans.draft
+    @user_plans_in_progress = current_user.user_plans.in_progress
+  end
 end
