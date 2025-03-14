f475b059c0fd8f5b5c782a6a6fe808631389f6fc
m-miskevych <miskevychmarianna@gmail.com>
2025-03-14 20:01:59 +0100

    feat: client dashboard

--- app/models/user_plan.rb
+++ app/models/user_plan.rb
@@ -7,4 +7,9 @@ class UserPlan < ApplicationRecord
   validates :user_id, uniqueness: { scope: :plan_id, message: "už má tento plán priradený" }
 
   enum status: { draft: 0, in_progress: 1, done: 2 }
+
+  # Scopes status
+  scope :draft, -> { where(status: 0) }
+  scope :in_progress, -> { where(status: 1) }
+  scope :done, -> { where(status: 2) }
 end
